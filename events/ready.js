9df3522a089abd61916e0d426c871ba55dd59d29
Multarix <darien2105@gmail.com>
2019-01-12 14:00:49 +1000

    Automatically cache the last 5 messages in every channel on start.

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
+const colors = require("colors");
 module.exports = async client => {
-	const colors = require("colors");
 
 	await wait(1000);
 	const time = new Date();
@@ -10,6 +10,7 @@ module.exports = async client => {
 	if(client.user.id === "260372003310010368") client.user.setActivity("hide the sausage", { type: 'PLAYING' }).then(client.log("Now playing " + colors.white("[") + "hide the sausage" + colors.white("]"), "Activity"));
 	if(client.user.id === "304230184494563329") client.user.setActivity("debug simulator", { type: 'PLAYING' }).then(client.log("Now playing " + colors.white("[") + "debug simulator" + colors.white("]"), "Activity"));
 
+	client.channels.filter(c => c.type === "text" && c.memberPermissions(c.guild.me).has("VIEW_CHANNEL")).forEach(c => c.fetchMessages({ limit: 5 }));
 //    client.guilds.filter(g => !client.settings.has(g.id)).forEach(g => client.settings.set(g.id, client.config.defaultSettings));
 };
 
