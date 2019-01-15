fe04d060254732d5c782b54c69ee136e128cf8f1
Multarix <darien2105@gmail.com>
2019-01-15 17:55:09 +1000

    Now a proper word searcher thing

--- commands/urban.js
+++ commands/urban.js
@@ -1,9 +1,39 @@
+const ud = require("urban-dictionary");
+const Discord = require("discord.js");
 exports.run = (client, message, args, level) => {
-	const internet = args.join("+");
 
-	if(!internet) return message.channel.send("Usage: [urban](<..word>)", { code: "markdown" });
+	const bad = client.emojis.get("340357882606256137");
+	const good = client.emojis.get("340357918996299778");
 
-	message.channel.send(`http://www.urbandictionary.com/define.php?term=${internet}`).catch(console.error);
+	const define = args.join(" ");
+	ud.term(define).then(defined => {
+		const results = defined.entries;
+		const filter = /[[\]]/g;
+
+		const word = results[0].word.toProperCase();
+		const definition = results[0].definition.replace(filter, "");
+		const example = results[0].example.replace(filter, "");
+
+		if(message.channel.memberPermissions(message.guild.me).has("EMBED_LINKS")){
+			const embed = new Discord.RichEmbed()
+				.setAuthor(word)
+				.addField("Definition:", definition + `\n\u200b`, false)
+				.addField("Usage:", example, false)
+				.setFooter(message.author.tag, message.author.displayAvatarURL)
+				.setTimestamp();
+			return message.channel.send({ embed });
+		}
+		return message.channel.send(`**${word}**\n**Definition:**\n${definition}\n**Usage:**\n${example}`);
+	}).catch(e => {
+		if(message.channel.memberPermissions(message.guild.me).has("EMBED_LINKS")){
+			const embed = new Discord.RichEmbed()
+				.addField(`Error ${bad}`, `Couldn't find a definition for the word \`${args[0]}\``)
+				.setFooter(message.author.tag, message.author.displayAvatarURL)
+				.setTimestamp();
+			return message.channel.send({ embed });
+		}
+		return message.channel.send(`**Error** ${bad}\nCouldn't find a definition for \`${define}\``);
+	});
 };
 
 exports.conf = {
