2fb0f170b7642dddcd6c698e153868f3fd20231c
Multarix <darien2105@gmail.com>
2019-08-26 20:26:43 +1000

    Minor Changes No more child-like server owner asking me for shit. Fuck you Connor if you're reading this.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,26 +7,16 @@ module.exports = async (client, member) => {
 		client.log(`Set "${member.user.tag}" to the default amount of points`, "SQL");
 	});
 
-	if(member.guild.id === "190317170478153729"){
-		const botDefault = member.guild.roles.get("234466818071199744");
-		const games = member.guild.roles.get("350584854246588416");
-		if(member.user.bot === true){
-			member.addRole(botDefault, "New Bot, pls don't replace me >.<");
-		} else {
-			member.addRoles(games, `New member, are you ready ${member.user.username}?`).catch(e => console.log(e.message));
-		}
-	}
 
 	if(member.guild.id === "237543420543893505"){
 		const botRole = member.guild.roles.get("237976002930671616");
 		const dj = member.guild.roles.get("462825089541341191");
-		const cmdbots = member.guild.roles.get("270836957456629761");
 		const newMem = member.guild.roles.get("237975739243298816");
 
 		if(member.user.bot === true){
 			member.addRole(botRole, "New Bot, pls don't replace me >.<");
 		} else {
-			member.addRoles([dj, newMem, cmdbots], `New member, are you ready ${member.user.username}?`).catch(console.error);
+			member.addRoles([dj, newMem], `New member, are you ready ${member.user.username}?`).catch(console.error);
 		}
 	}
 
