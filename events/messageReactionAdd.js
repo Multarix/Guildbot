0416b5e8df1f01fea5455f6ddf69c79f47178158
Multarix <darien2105@gmail.com>
2019-09-09 23:46:40 +1000

    Better reboot delete

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -5,7 +5,8 @@ module.exports = async (client, messageReaction, user) => {
 	if(user.bot) return;
 	const message = messageReaction.message;
 	const reaction = messageReaction;
-	if(message.content.toLowerCase() === "!reboot" && user.id === client.config.ownerID && reaction.emoji.id === "340357918996299778") message.delete();
+	const regPrefix = /[!\/]reboot/g;	// eslint-disable-line no-useless-escape
+	if(message.content.toLowerCase().match(regPrefix) && user.id === client.config.ownerID && reaction.emoji.id === "340357918996299778") message.delete();
 
 	const data = await sql.get(`SELECT * FROM settings WHERE guild = "${message.guild.id}"`);
 
