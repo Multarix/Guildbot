a669ad27e3881fdd1b2f23fcfd5a43dd1c5dc47e
Multarix <darien2105@gmail.com>
2019-09-10 15:31:16 +1000

    Stop erroring

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -6,7 +6,7 @@ module.exports = async (client, messageReaction, user) => {
 	const message = messageReaction.message;
 	const reaction = messageReaction;
 	const regPrefix = /[!\/]reboot/g;	// eslint-disable-line no-useless-escape
-	if(message.content.toLowerCase().match(regPrefix) && user.id === client.config.ownerID && reaction.emoji.id === "340357918996299778") message.delete();
+	if(message.content.toLowerCase().match(regPrefix) && user.id === client.config.ownerID && reaction.emoji.id === "340357918996299778") return message.delete().catch(e => { return; });
 
 	const data = await sql.get(`SELECT * FROM settings WHERE guild = "${message.guild.id}"`);
 
