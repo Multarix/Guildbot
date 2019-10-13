004f1fe218fe767f447ec9560acaacf2288026f0
Multarix <darien2105@gmail.com>
2019-10-13 12:00:42 +1000

    Better async awaited

--- commands/custom.js
+++ commands/custom.js
@@ -1,5 +1,6 @@
 const sql = require("sqlite");
 const Discord = require("discord.js");
+const delMsg = require("./config/delMsg.js");
 exports.run = async (client, message, args) => {
 
 	const cc = await sql.all(`SELECT * FROM commands WHERE guild = "${message.guild.id}"`);
@@ -16,18 +17,15 @@ exports.run = async (client, message, args) => {
 
 	const tag = args[0];
 	if(!tag){
-		if(message.channel.permissionsFor(message.guild.me).has("EMBED_LINKS")){
-			const embed = new Discord.MessageEmbed()
-				.setColor(14487568)
-				.addField("Custom Commands", cmdStr, false);
-			if(message.guild.iconURL){
-				embed.setAuthor(message.guild.name, message.guild.iconURL);
-			} else {
-				embed.setAuthor(message.guild.name);
-			}
-
-			return message.channel.send({ embed });
+		const embed = new Discord.MessageEmbed()
+			.setColor(14487568)
+			.addField("Custom Commands", cmdStr, false);
+		if(message.guild.iconURL){
+			embed.setAuthor(message.guild.name, message.guild.iconURL);
+		} else {
+			embed.setAuthor(message.guild.name);
 		}
+		return message.channel.send({ embed });
 	}
 
 	if(tag === "set" || tag === "add"){
@@ -38,28 +36,18 @@ exports.run = async (client, message, args) => {
 		if(!commandName.length > 15) return message.channel.send("Command name is too long. It must be 15 characters or less in length.");
 
 		if(cmdList[args[1]]){
-			return sql.get(`UPDATE commands SET output = "${joinOutput}" WHERE guild = "${message.guild.id}" AND name = "${commandName}"`).then(() => {
-				client.log(`"${message.guild.name}" updated a custom command (${commandName})`, `SQL`);
-				message.channel.send(`Updated the custom command: \`${commandName}\``).then(m => {
-					if(message.channel.permissionsFor(message.guild.me).has("MANAGE_MESSAGES")){
-						m.delete({ timeout: 10000 });
-						message.delete({ timeout: 10000 });
-					}
-				});
-			});
+			await sql.get(`UPDATE commands SET output = "${joinOutput}" WHERE guild = "${message.guild.id}" AND name = "${commandName}"`);
+			client.log(`"${message.guild.name}" updated a custom command (${commandName})`, `SQL`);
+			const m = await message.channel.send(`Updated the custom command: \`${commandName}\``);
+			return await delMsg(client, message, m);
 		}
 		const cockBlock = client.commands.get(commandName) || client.commands.get(client.aliases.get(commandName));
 		if(cockBlock) return message.channel.send("Cannot make a custom command with the same name/ alias as another command.");
 
-		return sql.run(`INSERT INTO commands (name, output, guild) VALUES ("${commandName}", "${joinOutput}", "${message.guild.id}")`).then(() => {
-			client.log(`"${message.guild.name}" added a custom command (${commandName})`, `SQL`);
-			message.channel.send(`Added the custom command: \`${commandName}\``).then(m => {
-				if(message.channel.permissionsFor(message.guild.me).has("MANAGE_MESSAGES")){
-					m.delete({ timeout: 10000 });
-					message.delete({ timeout: 10000 });
-				}
-			});
-		});
+		await sql.run(`INSERT INTO commands (name, output, guild) VALUES ("${commandName}", "${joinOutput}", "${message.guild.id}")`);
+		client.log(`"${message.guild.name}" added a custom command (${commandName})`, `SQL`);
+		const m = await message.channel.send(`Added the custom command: \`${commandName}\``);
+		return await delMsg(client, message, m);
 	}
 
 	if(tag === "delete" || tag === "remove"){
@@ -67,15 +55,10 @@ exports.run = async (client, message, args) => {
 		if(!commandName) return message.channel.send("No name was specified\nUsage: [custom](delete) <name>", { code: "markdown" });
 		const check = cc.find(c => c.name === commandName);
 		if(!check) return message.channel.send("That command doesn't exist");
-		return sql.run(`DELETE FROM commands WHERE name = "${commandName}" AND guild = "${message.guild.id}"`).then(() => {
-			client.log(`"${message.guild.name}" deleted a custom command (${commandName})`, `SQL`);
-			message.channel.send(`The custom command \`${commandName}\` has been deleted.`).then(m => {
-				if(message.channel.permissionsFor(message.guild.me).has("MANAGE_MESSAGES")){
-					m.delete({ timeout: 10000 });
-					message.delete({ timeout: 10000 });
-				}
-			});
-		});
+		await sql.run(`DELETE FROM commands WHERE name = "${commandName}" AND guild = "${message.guild.id}"`);
+		client.log(`"${message.guild.name}" deleted a custom command (${commandName})`, `SQL`);
+		const m = await message.channel.send(`The custom command \`${commandName}\` has been deleted.`);
+		return await delMsg(client, message, m);
 	}
 	return message.channel.send(`Usage: [custom](set/delete) <name> <..output>`, { code: "markdown" });
 };
