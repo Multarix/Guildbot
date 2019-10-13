8a24ffba892c72bbc0e752d46669b7a47f383e3e
Multarix <darien2105@gmail.com>
2019-10-13 19:23:01 +1000

    geyness factor now uses a formula to predict your gayness

--- commands/gay.js
+++ commands/gay.js
@@ -4,14 +4,31 @@ exports.run = async (client, message, args) => {
 	let geyPerson = message.author;
 	if(tagged) geyPerson = tagged;
 
-	if(geyPerson.id === "336709922647441409") return message.channel.send(`\`${geyPerson.username}\` is ${Math.floor(Math.random() * 100) + 100}% gey!`);
-	return message.channel.send(`\`${geyPerson.username}\` is ${Math.floor(Math.random() * 100)}% gey!`);
+	// if(geyPerson.id === "336709922647441409") return message.channel.send(`\`${geyPerson.username}\` is ${Math.floor(Math.random() * 100) + 100}% gey!`);
+	const userArray = geyPerson.username.split("");
+	let geyPercent = 0;
+	userArray.forEach(char => {
+		if(char.match(/[a-z]/)) return geyPercent += 1;
+		if(char.match(/[A-Z]/)) return geyPercent += 2;
+		if(char.match(/[1-9]/)) return geyPercent += parseInt(/[1-9]/.exec(char)[0]);
+		return geyPercent += 5;
+	});
 
+	const idArray = geyPerson.id.split("").slice(1);
+	let num = 0;
+	while(num === 0){
+		if(!idArray || idArray.length === 0){ num = 1; continue; }
+		if(parseInt(idArray[0]) === 0){ idArray.slice(1); continue; }
+		num = parseInt(idArray[0]);
+	}
+	geyPercent *= num;
+
+	return message.channel.send(`\`${geyPerson.username}\` is ${geyPercent}% gey!`);
 };
 
 exports.conf = {
 	enabled: true,
-	guildOnly: true,
+	allowDM: true,
 	aliases: ["gey"],
 	permLevel: 1,
 };
