23a621e82d521640c3a0652f42bec178c0f8d5ec
Multarix <darien2105@gmail.com>
2019-10-15 18:57:32 +1000

    updated guildDelete event.

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,15 +1,11 @@
 module.exports = async (client, guild) => {
 
-	const delSettings = sql.prepare();
-
-	sqlGet(`DELETE FROM settings WHERE guild = ?`, guild.id);
 	client.log(`Left the "${guild.name}" (${guild.id}) server`, "Notify");
 
-	const data = sqlGet(`SELECT * FROM points WHERE guild = ?`, guild.id);
-	if(!data) return client.log(`Found nobody from the "${guild.name}" server, skipping delete`, "SQL");
-
-	sqlAll(`DELETE FROM points WHERE guild = ?`, guild.id);
-	client.log(`Removed all users from the "${guild.name}" server`, "SQL");
+	sqlRun(`DELETE FROM settings WHERE guild = ?`, guild.id);
+	sqlRun(`DELETE FROM points WHERE guild = ?`, guild.id);
+	sqlRun("DELETE FROM commands WHERE guild = ?", guild.id);
+	client.log(`Removed all traces of "${guild.name}" (${guild.id}) from the database.`, "SQL");
 };
 
 module.exports.help = {
