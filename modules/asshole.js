6e7a2abf32d7c5e9752ff38b357a0b041822665d
Multarix <darien2105@gmail.com>
2020-01-18 20:21:50 +1000

    ily

--- modules/asshole.js
+++ modules/asshole.js
@@ -1,4 +1,5 @@
 module.exports = (client) => {
 	const member = client.guilds.get("237543420543893505").members.random();
-	client.channels.get("237543420543893505").send(`${member} faggot`).catch(e => { return; });
+	const msg = Math.random() ? "Faggot" : "I love you";
+	client.channels.get("237543420543893505").send(`${member} ${msg}`).catch(e => { return; });
 };
