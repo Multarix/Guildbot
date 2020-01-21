476212d8e099f0b8aa2ed921b857a72cf323eda1
Multarix <darien2105@gmail.com>
2020-01-21 14:31:52 +1000

    msg can now me faggot or ily

--- modules/asshole.js
+++ modules/asshole.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
 	const member = client.guilds.get("237543420543893505").members.random();
-	const msg = Math.random() ? "Faggot" : "I love you";
+	const msg = (Math.random() > 0.5) ? "faggot" : "I love you";
 	client.channels.get("237543420543893505").send(`${member} ${msg}`).catch(e => { return; });
 };
