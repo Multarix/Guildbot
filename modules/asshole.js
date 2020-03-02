adba839068f108926ad0182b4ce12146f02c148f
Multarix <darien2105@gmail.com>
2020-03-02 18:23:08 +1000

    Fixed Random Ping. (Maybe?)

--- modules/asshole.js
+++ modules/asshole.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
-	const member = client.guilds.cache.get("237543420543893505").members.random();
+	const member = client.guilds.cache.get("237543420543893505").members.cache.random();
 	const msg = (Math.random() > 0.5) ? "faggot" : "I love you";
 	client.channels.cache.get("237543420543893505").send(`${member} ${msg}`).catch(e => { return; });
 };
