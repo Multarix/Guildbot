6fd054d4b8759870ab44c82f0bb683485726a96e
Multarix <darien2105@gmail.com>
2020-03-05 10:14:52 +1000

    Added output within discord

--- commands/exec.js
+++ commands/exec.js
@@ -1,9 +1,35 @@
 exports.run = async (client, message, args) => {
+	const Discord = require('discord.js');
+	const embed = new Discord.MessageEmbed()
+		.setFooter(client.user.tag, client.user.displayAvatarURL())
+		.setTimestamp();
+
 	const joinArgs = args.join(" ");
-	client.log(joinArgs, "Exec");
+	client.log(joinArgs, "Exec Input");
+
 	require("child_process").exec(joinArgs, (error, result) => {
-		if(error) return client.log(error.message, "Exec Output");
-		if(result) return client.log(`${result}`, "Exec Output");
+		if(error){
+			// Error
+			client.log(`\n\`\`\`fix${error.message}\`\`\``, "Exec Output");
+			const failString = `\`\`\`fix
+				Input: ${joinArgs}
+				Output:
+				${error.message}
+				\`\`\``;
+			if(failString.length >= 2000) return message.channel.send("```fix\nThe command execution failed.\nThe output was too long - Check the console for results.\n```");
+			return message.channel.send(failString.removeIndents());
+		}
+		if(result){
+			// Success
+			client.log(`\n${result}`, "Exec Output");
+			const successString = `\`\`\`yaml
+				Input: ${joinArgs}
+				Output:
+				${result}
+				\`\`\``;
+			if(successString.length >= 2000) return message.channel.send("```yaml\nThe command executed successfully.\nThe output was too long - Check the console for results.\n```");
+			return message.channel.send(successString.removeIndents());
+		}
 	});
 };
 
