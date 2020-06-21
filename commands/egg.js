e4c15ebf228b7b8b5ec7ca7fbda5565dc2a5c1ec
Multarix <darien2105@gmail.com>
2020-06-21 20:43:36 +1000

    Shiny pokemon commands. I did say this bot was mainly private use right?

---
+++ commands/egg.js
@@ -0,0 +1,22 @@
+exports.run = (client, message, args) => {
+	const eggNumber = parseInt(args[0]);
+	if(!eggNumber) return message.channel.send("Not a number, please try again");
+
+	const percentage = Math.round((1 - Math.pow((511 / 512), eggNumber)) * 100000) / 1000;
+	message.channel.send(`There is a ${percentage}% chance you should have hatched a shiny after ${eggNumber} eggs.`);
+};
+
+
+exports.conf = {
+	enabled: true,
+	allowDM: true,
+	aliases: ["eggs"],
+	permLevel: 1
+};
+
+exports.help = {
+	name: "egg",
+	category: "Misc",
+	description: "Calculates the chance that a shiny pokemon should have hatched in a given number of eggs",
+	usage: "egg-number"
+};
---
+++ commands/hatch.js
@@ -0,0 +1,57 @@
+exports.run = (client, message, args) => {
+	const Discord = require('discord.js');
+	const embed = new Discord.MessageEmbed()
+		.setTitle(`Masuda Method Breeding`)
+		.setFooter(client.user.tag, client.user.displayAvatarURL())
+		.setTimestamp();
+
+	let num = parseInt(args[0]);
+	let rounds = parseInt(args[1]);
+
+	if(!num) return message.channel.send("Not a Number, please try again");
+	if(!rounds) rounds = 1;
+
+
+	if(num > 5000) num = 5000;
+	if(rounds > 21) rounds = 21;
+
+	embed.setDescription(`Eggs Per Attempt: ${num}\nTotal Eggs: ${num * rounds}`);
+
+	let average = 0;
+	let totalShinyCount = 0;
+	for(let i = 0; i < rounds; i++){
+		let shinyCount = 0;
+		let shinyMessage = "";
+
+		for(let ii = 0; ii < num; ii++){
+
+			const shinyCheck = Math.floor(Math.random() * 512 + 1);
+			if(shinyCheck === 1){
+				shinyCount += 1;
+				shinyMessage += `Shiny #${shinyCount}: Egg #${ii + 1}\n`;
+			}
+		}
+		average += shinyCount;
+		embed.addField(`Round #${i + 1}`, `Shinies \:star:: ${shinyCount}`, true); /* eslint-disable-line no-useless-escape */
+	}
+	totalShinyCount = average;
+	average = Math.round((average * 100) / rounds) / 100;
+	embed.addField("Stats", `Average: ${average}\:star:\n Total: ${totalShinyCount}\:star:`, false); /* eslint-disable-line no-useless-escape */
+
+	message.channel.send({ embed });
+};
+
+
+exports.conf = {
+	enabled: true,
+	allowDM: true,
+	aliases: ["eh", "hatch"],
+	permLevel: 1
+};
+
+exports.help = {
+	name: "hatch",
+	category: "Misc",
+	description: "Hatches pokemon eggs using Masuda Method + Shiny Charm calculations to get shinies",
+	usage: "Amount-Of-Eggs Attempts"
+};
