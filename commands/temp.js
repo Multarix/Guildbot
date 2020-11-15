b6dd98d84ce61955afa071d74feeb290ce55cef1
Multarix <darien2105@gmail.com>
2020-11-15 17:47:14 +1000

    Rounding

--- commands/temp.js
+++ commands/temp.js
@@ -28,8 +28,14 @@ exports.run = async (client, message, args) => {
 	const temp = parseInt(args[1]);
 	if(isNaN(temp)) return message.channel.send("The 2nd argument is not a number, please try again");
 
-	const convertFromCelsius = (num) => ((num / 5) * 9) + 32;
-	const convertToCelsius = (num) => ((num - 32) * 5) / 9;
+	const round = (num) => {
+		const multiplier = Math.pow(10, 2);
+		return Math.round(num * multiplier) / multiplier;
+	};
+
+	const convertFromCelsius = (num) => round(((num / 5) * 9) + 32);
+	const convertToCelsius = (num) => round(((num - 32) * 5) / 9);
+
 
 	const convertedTemp = (temp1Type === "°C") ? convertToCelsius(temp) : convertFromCelsius(temp);
 	return message.channel.send(`${temp}${temp1Type} is ${convertedTemp}${temp2Type}`);
