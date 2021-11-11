f09018634b7c3d1fa499db3f2bb69fbe3b29c77f
Multarix <darien2105@gmail.com>
2021-11-12 00:39:25 +1000

    Shouldn't trigger multiple days in a row Also has a slight issue in that if the bot was the last person to speak in the channel, it won't post a message regardless

--- modules/asshole.js
+++ modules/asshole.js
@@ -2,5 +2,9 @@ const responses = require("./assholeResponses.json");
 module.exports = (client, guildID) => {
 	const member = client.guilds.cache.get(guildID).members.cache.filter(m => m.user.id !== client.user.id).random();
 	const msg = responses[Math.floor(Math.random() * responses.length)];
-	client.channels.cache.get("237543420543893505").send(`${member} ${msg}`).catch(e => { return; });
+
+	const lstMsg = client.channels.cache.get("237543420543893505").lastMessage;
+	if(lstMsg.author.id !== client.user.id){
+		client.channels.cache.get("237543420543893505").send(`${member} ${msg}`).catch(e => { return; });
+	}
 };
