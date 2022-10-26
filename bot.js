66a1a9596cde21d8c7152f514b6439587c89050a
Multarix <darien2105@gmail.com>
2022-10-26 11:40:19 +1000

    Book updates

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
+const fs = require("fs");
+const { request } = require("http");
 const cron = require("node-cron");
 
 const { promisify } = require("util");
-const readdir = promisify(require("fs").readdir);
+const readdir = promisify(fs.readdir);
 
 const Intents = Discord.Intents.FLAGS;
 const client = new Discord.Client({
@@ -59,6 +61,34 @@ const init = async () => {
 
 	cron.schedule("0 0 12 * * *", async () => {
 		if(client.config.dailyMessage) client.asshole(client, client.config.homeServer);
+	});
+
+	// Every hour, check for book updates
+	cron.schedule("0 * * * *", async () => {
+		let last = fs.readFileSync("./objects/last.json", "utf8");
+
+		if(!last) last = '{ "items": [{ "title": "No items found" }] }';
+
+
+		const lastObj = JSON.parse(last);
+
+		const url = "https://labs.j-novel.club/feed/user/629784d74efdb04c77f8ea67.json";
+		let newData = "";
+		request(url, function(error, response, body){
+			if(!error && response.statusCode == 200){
+				newData = JSON.parse(body);
+			}
+		});
+
+		// Check if the latest chapter is the same as the last one
+		if(newData.items[0].title !== lastObj.items[0].title){
+			// Send a message to the main channel of the home server
+
+			const textMessage = `${newData.items[0].title} is out!\n${newData.items[0].url}`;
+			client.channels.cache.get("1034640596146602024").send(textMessage).catch(e => { return; });
+
+			fs.writeFileSync("./objects/last.json", JSON.stringify(last));
+		}
 	});
 };
 
