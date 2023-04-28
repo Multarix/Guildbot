b52ed6932d18fb9878eb89896b9f42d5d25d0688
Multarix <darien2105@gmail.com>
2023-04-28 20:16:25 +1000

    Slash command changes to allow for arguments

--- commands/info.js
+++ commands/info.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder, Client, Message, Interaction, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, Client, Message, ChatInputCommandInteraction, EmbedBuilder } = require("discord.js");
 
 
 /**
  * @name info
  * @param {Client} client The discord client
- * @param {Message|Interaction} element The message or interaction that was created
+ * @param {Message|ChatInputCommandInteraction} element The message or interaction that was created
  * @param {String[]} _args The arguments passed to the command
  * @returns {Promise<void>}
 **/
@@ -31,6 +31,7 @@ async function run(client, element, _args = []){
 const info = {
 	name: "info",
 	description: "Gives some useful bot statistics",
+	usage: "info",
 	enabled: false,
 	altNames: ["stats"],
 	dmCompatible: false,
@@ -38,14 +39,33 @@ const info = {
 	category: "System"
 };
 
-const slash = {
-	data: new SlashCommandBuilder()
-		.setName(info.name)
-		.setDescription(info.description)
-		.setDMPermission(false),
-	async execute(client, interaction){
-		await run(client, interaction);
+
+/**
+ * @name slash
+ * @param {Client} client The discord client
+ * @param {Boolean} [funcs=false] Whether to return the functions or the data
+ * @returns {Object} The slash command data or functions
+**/
+function slash(client, funcs = false){
+	if(!funcs){ // We don't want to get the functions
+		return {
+			data: new SlashCommandBuilder()
+				.setName(info.name)
+				.setDescription(info.description)
+				.setDMPermission(false)
+		};
 	}
-};
+
+	return {
+		/**
+		 * @name execute
+		 * @param {ChatInputCommandInteraction} interaction The interaction that was created
+		 * @description The function that is called when the slash command is used
+		**/
+		execute: async function execute(interaction){
+			await run(client, interaction);
+		}
+	};
+}
 
 module.exports = { run, slash, info };
