11dbc3178c30ea7ff6915063e6f5861322b89a3e
Multarix <darien2105@gmail.com>
2023-05-01 17:48:04 +1000

    add reboot command

---
+++ commands/reboot.js
@@ -0,0 +1,63 @@
+const { Client, Message } = require("discord.js");
+const { output } = require("../src/functions.js");
+
+
+/**
+ * @name reboot
+ * @param {Client} client The discord client
+ * @param {Message} element The message or interaction that was created
+ * @param {String[]} _args The arguments passed to the command
+ * @returns {Promise<void>}
+**/
+async function run(client, element, _args = []){
+	let good = client.emojis.cache.get("340357918996299778");
+	if(!good) good = "👍";
+
+	output("misc", "Perfmorming manual reboot...");
+	await element.react(good);
+
+	process.exit();
+}
+
+
+const info = {
+	name: "reboot",
+	description: "(Owner Only) Restarts the bot",
+	usage: "reboot",
+	enabled: true,
+	altNames: ["restart"],
+	dmCompatible: true,
+	permLevel: 100,
+	category: "System"
+};
+
+
+/**
+ * @name slash
+ * @param {Client} client The discord client
+ * @param {Boolean} [funcs] Whether to return the functions or the data
+ * @returns {Object} The slash command data or functions
+**/
+function slash(client, funcs = false){
+	// if(!funcs){ // We want to get the slash command data
+	// 	return {
+	// 		data: new SlashCommandBuilder()
+	// 			.setName(info.name)
+	// 			.setDescription(info.description)
+	// 			.setDMPermission(false)
+	// 	};
+	// }
+
+	return {
+		/**
+		 * @name execute
+		 * @param {ChatInputCommandInteraction} interaction The interaction that was created
+		 * @description The function that is called when the slash command is used
+		**/
+		execute: async function execute(interaction){
+			await run(client, interaction);
+		}
+	};
+}
+
+module.exports = { run, slash, info };
