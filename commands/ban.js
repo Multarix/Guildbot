6fc493cf572333edf91623481555fd695c2051d2
Multarix <darien2105@gmail.com>
2023-05-01 14:04:57 +1000

    Add new commands ban, kick, eval & exec

---
+++ commands/ban.js
@@ -0,0 +1,163 @@
+const {
+	GuildMember,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	SlashCommandBuilder,
+	Client,
+	Message,
+	ChatInputCommandInteraction,
+	EmbedBuilder,
+	PermissionsBitField,
+	PermissionFlagsBits
+} = require("discord.js");
+const { handleElement } = require("../handlers/elementHandler");
+
+// Buttons for interactions
+const confirm = new ButtonBuilder().setStyle(ButtonStyle.Danger).setLabel("Ban User").setCustomId("ban_confirm");
+const cancel = new ButtonBuilder().setStyle(ButtonStyle.Secondary).setLabel("Cancel").setCustomId("ban_cancel");
+const buttons = new ActionRowBuilder().addComponents(cancel, confirm);
+
+
+/**
+ * @name handleComponent;
+ * @param {Message} interactionReply
+ * @param {ChatInputCommandInteraction} inputCommand
+ * @param {EmbedBuilder} embed
+ * @param {GuildMember} kickMember
+ * @returns {Promise<Message>}
+**/
+async function handleComponent(interactionReply, inputCommand, embed, banUser, reason){
+
+	// Filter
+	const f = (i) => {
+		const userID = inputCommand.user?.id || inputCommand.author?.id;
+		return i.user.id === userID;
+	};
+
+	// Functions for editing the embed
+	const successKick = e => e.spliceFields(0, 1).setTitle(`Successfully banned \`${banUser.tag}\``);
+	const cancelKick = e => e.spliceFields(0, 2).setTitle("Command canceled");
+	const timeOut = e => e.spliceFields(0, 2).setTitle("Command timed out");
+	const failBan = e => e.spliceFields(0, 2).setTitle(`Failed to ban \`${banUser.tag}\``);
+
+	try {
+		const res = await interactionReply.awaitMessageComponent({ filter: f, time: 30000 });
+
+		if(res.customId === "ban_cancel") return await res.update({ content: "", embeds: [cancelKick(embed)], components: [] });
+		if(res.customId === "ban_confirm"){
+			let failed = false;
+			inputCommand.guild.members.ban(banUser, { reason }).catch(() => { failed = true; });
+
+			if(failed) return await res.update({ content: "", embeds: [failBan(embed)], components: [] });
+			return await res.update({ content: "", embeds: [successKick(embed)], components: [] });
+		}
+
+	} catch (err){
+		await interactionReply.edit({ content: "", embeds: [timeOut(embed)], components: [] });
+	}
+}
+
+
+/**
+ * @name ban
+ * @param {Client} client The discord client
+ * @param {Message|ChatInputCommandInteraction} element The message or interaction that was created
+ * @param {String[]} args The arguments passed to the command
+ * @returns {Promise<void>}
+**/
+async function run(client, element, args = []){
+
+	const isSlashCommand = (element.type === 2) ? true : false;
+	// if(isSlashCommand) await element.deferReply({ ephemeral: true }); // Defering breaks things
+
+	const user = (isSlashCommand) ? element.user : element.author;
+
+	// Check if the bot can even kick
+	if(!element.channel.permissionsFor(element.guild.members.me).has(PermissionsBitField.Flags.BanMembers)) return await handleElement(element, isSlashCommand, { content: "I do not have permission to kick members.", ephemeral: true });
+
+	// Grab the user to kick
+	let banUser = args.shift();
+	if(!isSlashCommand) banUser = await grabUser(banUser);
+	if(!banUser) return await handleElement(element, isSlashCommand, { content: "No User/ Invalid User.\nPlease Specify a user to ban", ephemeral: true });
+
+	// Check if the user is in the server
+	const banMember = element.guild.members.cache.get(banUser.id);
+	// Check if the user is unbannable for some reason
+	if(banMember && !banMember.kickable) return await handleElement(element, isSlashCommand, { content: "I am unable to ban that user.", ephemeral: true });
+
+	// Grab the reason
+	let reason = args.join(" ");
+	if(!reason) reason = "Not Specified";
+
+	// Create the embed
+	const embed = new EmbedBuilder()
+		.setAuthor({ name: `${banUser.tag} (${banUser.id})`, iconURL: banUser.displayAvatarURL() })
+		.setColor(16750080)
+		.setFooter({ text: user.tag, iconURL: user.displayAvatarURL() })
+		.setTimestamp();
+
+	const fields = [
+		{ name: "Action:", value: "Ban", inline: false },
+		{ name: "Reason:", value: reason, inline: false }
+	];
+
+	embed.addFields(fields);
+
+	const msg = await handleElement(element, isSlashCommand, { embeds: [embed], components: [buttons], ephemeral: true });
+	return handleComponent(msg, element, embed, banUser, reason);
+}
+
+
+const info = {
+	name: "ban",
+	description: "Kicks a user from the server",
+	usage: "ban <user> {reason}",
+	enabled: true,
+	altNames: [],
+	dmCompatible: false,
+	permLevel: 0,
+	category: "Moderation"
+};
+
+
+/**
+ * @name slash
+ * @param {Client} client The discord client
+ * @param {Boolean} [funcs] Whether to return the functions or the data
+ * @returns {Object} The slash command data or functions
+**/
+function slash(client, funcs = false){
+	if(!funcs){ // We want to get the slash command data
+		return {
+			data: new SlashCommandBuilder()
+				.setName(info.name)
+				.setDescription(info.description)
+				.setDMPermission(false)
+				.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+				.addUserOption(option => option.setRequired(true).setName("user").setDescription("The user to ban"))
+				.addStringOption(option => option.setRequired(false).setName("reason").setDescription("The reason for banning the user"))
+		};
+	}
+
+	return {
+		/**
+		 * @name execute
+		 * @param {ChatInputCommandInteraction} interaction The interaction that was created
+		 * @description The function that is called when the slash command is used
+		**/
+		execute: async function execute(interaction){
+			const args = [];
+
+			const user = interaction.options.getUser("user");
+			args.push(user);
+
+			const reason = interaction.options.getString("reason");
+			if(reason) args.push(reason);
+
+			await run(client, interaction, args);
+		}
+	};
+}
+
+module.exports = { run, slash, info };
---
+++ commands/eval.js
@@ -0,0 +1,98 @@
+const { Client, Message, ChatInputCommandInteraction, EmbedBuilder } = require("discord.js");
+
+
+/**
+ * @name eval
+ * @param {Client} client The discord client
+ * @param {Message} element The message or interaction that was created
+ * @param {String[]} args The arguments passed to the command
+ * @returns {Promise<void>}
+**/
+async function run(client, element, args){
+
+	let good = client.emojis.cache.get("340357918996299778");
+	if(!good) good = "👍";
+	let bad = client.emojis.cache.get("340357882606256137");
+	if(!bad) bad = "👎";
+
+	const embed = new EmbedBuilder()
+		.setFooter({ text: client.user.tag, iconURL: client.user.displayAvatarURL() })
+		.setTimestamp();
+
+	const code = args.join(" ").replace(/\u200b/g, "\n");
+	try {
+		const evaled = eval(code);
+		const clean = await client.clean(client, evaled);
+		const evalString = `**OUTPUT** ${good}\n\`\`\`javascript\n${clean}\n\`\`\``;
+		if(evalString.length >= 1024){
+			console.log(clean);
+			return await element.channel.send(`**OUTPUT** ${good}\nThe output was too long, check the console.`);
+		}
+
+		embed.setColor(2734377)
+			.addFields({ title: 'Javascript Evaluated', value: evalString, inline: false });
+
+		return await element.channel.send({ embeds: [embed] });
+
+	} catch (err){
+		const errMsg = await client.clean(client, err);
+		const errString = `**ERROR** ${bad}\n\`\`\`javascript\n${errMsg}\n\`\`\``;
+		if(errString.length >= 1024){
+			console.log(errString);
+			return await element.channel.send(`**ERROR** ${bad}\nThe error message was too long, check the console.`);
+		}
+
+		embed.setColor(14487568)
+			.addFields({ title: `Javascript Evaluated`, value: errString, inline: false });
+
+		return await element.channel.send({ embeds: [embed] });
+	}
+}
+
+
+const info = {
+	name: "eval",
+	description: "Evaluates Javascript (Owner Only)",
+	usage: "eval {code}",
+	enabled: true,
+	altNames: ["e", "js"],
+	dmCompatible: true,
+	permLevel: 100,
+	category: "System"
+};
+
+
+/**
+ * @name slash
+ * @param {Client} client The discord client
+ * @param {Boolean} [funcs] Whether to return the functions or the data
+ * @returns {Object} The slash command data or functions
+**/
+function slash(client, funcs = false){
+	// if(!funcs){ // We want to get the slash command data
+	// 	return {
+	// 		data: new SlashCommandBuilder()
+	// 			.setName(info.name)
+	// 			.setDescription(info.description)
+	// 			.setDMPermission(false)
+	// 	};
+	// }
+
+	return {
+		/**
+		 * @name execute
+		 * @param {ChatInputCommandInteraction} interaction The interaction that was created
+		 * @description The function that is called when the slash command is used
+		**/
+		execute: async function execute(interaction){
+			const user = interaction.options.getUser("user");
+
+			const args = [];
+			if(user) args.push(user);
+
+			await run(client, interaction, args);
+		}
+	};
+}
+
+module.exports = { run, slash, info };
