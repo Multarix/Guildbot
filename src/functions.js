faa8bede8a662a488e964b0ae5337eb772ff8779
Multarix <darien2105@gmail.com>
2023-05-01 14:06:43 +1000

    Function file merge add humanTIme, ordinal & timeFormat to ./src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,165 +1,390 @@
+const { User, Channel, Client, Emoji, PermissionsBitField, Embed } = require("discord.js");
 const colors = require("colors");
-const time = require("../modules/timeFormat.js");
-const { User, Channel, Client, Emoji, PermissionsBitField } = require("discord.js");
+
 
 /**
- * @name functions
- * @param {Client} client The discord client object
- * @description A collection of functions used throughout the bot
+ * @name ordinal
+ * @param {Number|String} num The number to be converted to an ordinal format
+ * @returns {String} The number in ordinal format
+ * @description Convert a number to an ordinal format (1st, 2nd, 3rd, 4th.. etc.)
+ * @example const ord = ordinal(53);
+ * console.log(ord); // 53rd
 **/
-module.exports = (client) => {
+function ordinal(num){
+	// Ordinal indicators (1st, 2nd, 3rd, 4th.. etc.)
+	if(isNaN(num)) return 'NaN';
+
+	const ord = num.toString();
+	if(ord.endsWith("1") && !ord.endsWith("11")) return `${ord}st`;
+	if(ord.endsWith("2") && !ord.endsWith("12")) return `${ord}nd`;
+	if(ord.endsWith("3") && !ord.endsWith("13")) return `${ord}rd`;
+
+	return `${ord}th`;
+}
 
 
-	/**
-	 * @name output
-	 * @param {string} type Possible Values: good, warn, info, misc, error, normal
-	 * @param {any} message The item to be logged
-	 * @returns {void}
-	 * @description Logs a message to the console
-	 * @example client.output("normal", "This is a message");
-	 * // [2020-12-31 | 11:59:59] This is a message
-	**/
-	client.output = function output(type, message){
-		const curTime = time();
-		switch(type.toLowerCase()){
-			case "good":
-				console.log(`[${colors.gray(`${curTime.date} | ${curTime.time}`)}] ${colors.green(message)}`);
-				break;
+/**
+ * @typedef {object} timeObject
+ * @property {string} day The day of the week
+ * @property {string} shortDay The day of the week in short form
+ * @property {string} month The month of the year
+ * @property {string} shortMonth The month of the year in short form
+ * @property {string} date The date in the format `DD/MM/YYYY`
+ * @property {string} fullDate The date in the format `DDth of Month, YYYY`
+ * @property {string} time The time in the format `HH:MM:SS`
+ * @property {string} ISO The time in the format `YYYY-MM-DDTHH:MM:SS.MSSZ`
+**/
 
-			case "warn":
-				console.log(`[${colors.gray(`${curTime.date} | ${curTime.time}`)}] ${colors.yellow(message)}`);
-				break;
+/**
+ * @name timeFormat
+ * @param {Date} [dateTime] The date object to be used, defaults to the current time
+ * @returns {timeObject} An object containing the time in different formats
+ * @description Returns the time in different formats
+ * - day: The day of the week
+ * - shortDay: The day of the week in short form
+ * - month: The month of the year
+ * - shortMonth: The month of the year in short form
+ * - date: The date in the format `DD/MM/YYYY`
+ * - fullDate: The date in the format `DDth of Month, YYYY`
+ * - time: The time in the format `HH:MM:SS`
+ * - ISO: The time in the format `YYYY-MM-DDTHH:MM:SS.MSSZ`
+ * @example const time = timeFormat();
+ * console.log(time.fullDate); // 25th of August, 2023
+**/
+function timeFormat(dateTime = new Date()){
 
-			case "error":
-				console.log(`[${colors.gray(`${curTime.date} | ${curTime.time}`)}] ${colors.red(message)}`);
-				break;
+	const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	const shortWeekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+	const months = [
+		'January',	 'February',
+		'March',	 'April',
+		'May', 		 'June',
+		'July',	 	 'August',
+		'September', 'October',
+		'November',	 'December'
+	];
 
-			case "info":
-			case "misc":
-				console.log(`[${colors.gray(`${curTime.date} | ${curTime.time}`)}] ${colors.cyan(message)}`);
-				break;
+	const shortMonths = [
+		'Jan', 'Feb',
+		'Mar', 'Apr',
+		'May', 'Jun',
+		'Jul', 'Aug',
+		'Sep', 'Oct',
+		'Nov', 'Dec'
+	];
 
-			case "normal":
-			default:
-				console.log(`[${colors.gray(`${curTime.date} | ${curTime.time}`)}] ${colors.white(message)}`);
-				break;
-		}
+	const day = weekdays[dateTime.getDay()];													// Tuesday
+	const shortDay = shortWeekdays[dateTime.getDay()];											// Tue
+	const month = months[dateTime.getMonth()];													// August
+	const shortMonth = shortMonths[dateTime.getMonth()];										// Aug
+	const date = `${dateTime.getDate()}/${dateTime.getMonth() + 1}/${dateTime.getFullYear()}`;	// 25/8/2023
+
+
+	const ordinalDay = ordinal(dateTime.getDate().toString());
+	const fullDate = `${ordinalDay} of ${month}, ${dateTime.getFullYear()}`;					// 25th of August, 2023
+
+
+	let hour = dateTime.getHours().toString();
+	if(dateTime.getHours() < 10) hour = "0" + dateTime.getHours();
+	let minute = dateTime.getMinutes().toString();
+	if(dateTime.getMinutes() < 10) minute = "0" + dateTime.getMinutes();
+	let second = dateTime.getSeconds().toString();
+	if(dateTime.getSeconds() < 10) second = "0" + dateTime.getSeconds();
+	const time = `${hour}:${minute}:${second}`;													// 12:30:15
+
+	return {
+		day,
+		shortDay,
+		month,
+		shortMonth,
+		date,
+		fullDate,
+		time,
+		ISO: dateTime.toISOString()																// 2023-08-25T12:30:15.000Z
 	};
+}
 
 
-	/**
-	 * @name permLevel
-	 * @param {User} user A discord user object
-	 * @param {Channel} channel A discord channel object
-	 * @returns {number}
-	 * @description Returns the permission level of the user
-	 * @example const permLevel = client.permLevel(client, message);
-	**/
-	client.permLevel = function permLevel(user, channel){
-		let perm = 0;
-		if(channel.type === "DM") return perm;
+/**
+ * @name output
+ * @param {string} type Possible Values: good, warn, info, misc, error, normal
+ * @param {any} message The item to be logged
+ * @returns {void}
+ * @description Logs a message to the console
+ * @example client.output("normal", "This is a message");
+ * // [2020-12-31 | 11:59:59] This is a message
+**/
+function output(type, message){
+	const curTime = timeFormat();
+	switch(type.toLowerCase()){
+		case "good":
+			console.log(`[${colors.gray(`${curTime.date} | ${curTime.time}`)}] ${colors.green(message)}`);
+			break;
 
-		const personCanDelete = channel.permissionsFor(channel.guild.members.cache.get(user.id)).has(PermissionsBitField.Flags.ManageMessages);
-		const personCanKick = channel.permissionsFor(channel.guild.members.cache.get(user.id)).has(PermissionsBitField.Flags.KickMembers);
-		const personCanBan = channel.permissionsFor(channel.guild.members.cache.get(user.id)).has(PermissionsBitField.Flags.BanMembers);
-		const personIsAdmin = channel.permissionsFor(channel.guild.members.cache.get(user.id)).has(PermissionsBitField.Flags.Administrator);
+		case "warn":
+			console.log(`[${colors.gray(`${curTime.date} | ${curTime.time}`)}] ${colors.yellow(message)}`);
+			break;
 
-		if(personCanDelete) perm = 2;
-		if(personCanKick) perm = 4;
-		if(personCanBan) perm = 6;
-		if(personIsAdmin) perm = 8;
+		case "error":
+			console.log(`[${colors.gray(`${curTime.date} | ${curTime.time}`)}] ${colors.red(message)}`);
+			break;
 
-		if(user.id === channel.guild.ownerId) perm = 10;
-		if(user.id === client.config.ownerID) perm = 100;
+		case "info":
+		case "misc":
+			console.log(`[${colors.gray(`${curTime.date} | ${curTime.time}`)}] ${colors.cyan(message)}`);
+			break;
 
-		return perm;
-	};
-
-
-	/**
-	 * @name clean
-	 * @param {string} text The text to be cleaned
-	 * @returns {Promise<string>}
-	 * @description Cleans the text of any sensitive information
-	 * @example const cleanText = client.clean(client, text);
-	**/
-	client.clean = async function clean(text){
-		if(text && text.constructor.name == "Promise") text = await text;
-		if(typeof evaled !== "string") text = require("util").inspect(text, { depth: 0 });
-
-		text = text
-			.replace(/`/g, "`" + String.fromCharCode(8203))
-			.replace(/@/g, "@" + String.fromCharCode(8203))
-			.replace(client.token, "https://i.imgur.com/cGIay9e.png");
-
-		return text;
-	};
+		case "normal":
+		default:
+			console.log(`[${colors.gray(`${curTime.date} | ${curTime.time}`)}] ${colors.white(message)}`);
+			break;
+	}
+}
 
 
-	/**
-	 * @name grabChannel
-	 * @param {string} channelID The ID of the channel to be grabbed
-	 * @returns {Promise<Channel | undefined>} The channel object or undefined if the channel is not found
-	 * @description Grabs a channel object from the cache or fetches it from the API
-	 * @example const channel = client.grabChannel("1234567890");
-	**/
-	client.grabChannel = async function grabChannel(channelID){
-		if(!channelID) return undefined;
-		if(channelID.startsWith("<#") && channelID.endsWith(">")) channelID = channelID.slice(2, -1);
-		if(client.channels.cache.get(channelID)) return client.channels.cache.get(channelID);
+/**
+ * @name permLevel
+ * @param {Client} client The client object
+ * @param {User} user A discord user object
+ * @param {Channel} channel A discord channel object
+ * @returns {number}
+ * @description Returns the permission level of the user
+ * @example const permLevel = client.permLevel(client, message);
+**/
+function permLevel(client, user, channel){
 
-		await client.channels.fetch(channelID).catch(e => { return undefined; });
+	let perm = 0;
+	if(channel.type === "DM") return perm;
 
-		return (client.channels.cache.get(channelID)) ? client.channels.cache.get(channelID) : undefined;
-	};
+	const personCanDelete = channel.permissionsFor(channel.guild.members.cache.get(user.id)).has(PermissionsBitField.Flags.ManageMessages);
+	const personCanKick = channel.permissionsFor(channel.guild.members.cache.get(user.id)).has(PermissionsBitField.Flags.KickMembers);
+	const personCanBan = channel.permissionsFor(channel.guild.members.cache.get(user.id)).has(PermissionsBitField.Flags.BanMembers);
+	const personIsAdmin = channel.permissionsFor(channel.guild.members.cache.get(user.id)).has(PermissionsBitField.Flags.Administrator);
 
+	if(personCanDelete) perm = 2;
+	if(personCanKick) perm = 4;
+	if(personCanBan) perm = 6;
+	if(personIsAdmin) perm = 8;
 
-	/**
-	 * @name grabUser
-	 * @param {string} userID The ID of the user to be grabbed
-	 * @returns {Promise<User | undefined>} The user object or undefined if the user is not found
-	 * @description Grabs a user object from the cache or fetches it from the API
-	 * @example const user = client.grabUser("1234567890");
-	 */
-	client.grabUser = async function grabUser(userID){
-		if(!userID) return undefined;
-		if(userID.startsWith("<@") && userID.endsWith(">")) userID = userID.slice(2, -1);
-		if(userID.startsWith("!")) userID = userID.slice(1);
+	if(user.id === channel.guild.ownerId) perm = 10;
+	if(user.id === client.config.ownerID) perm = 100;
 
-		if(client.users.cache.get(userID)) return client.users.cache.get(userID);
-
-		await client.users.fetch(userID).catch(e => { return undefined; });
-
-		return (client.users.cache.get(userID)) ? client.users.cache.get(userID) : undefined;
-	};
+	return perm;
+}
 
 
-	/**
-	 * @name grabEmoji
-	 * @param {string} emojiID The ID of the emoji to be grabbed
-	 * @returns {Promise<Emoji | undefined>} The emoji object or undefined if the emoji is not found
-	 * @description Grabs a emoji object from the cache via it's ID
-	 * @example const emoji = client.grabEmoji("1234567890");
-	 */
-	client.grabEmoji = async function grabEmoji(emojiID){
-		if(!emojiID) return undefined;
-		if(emojiID.startsWith("<a:") && emojiID.endsWith(">")) emojiID = emojiID.slice(3, -1);	// Animated
-		if(emojiID.startsWith("<:") && emojiID.endsWith(">")) emojiID = emojiID.slice(2, -1);	// Static
+/**
+ * @name clean
+ * @param {Client} client The client object
+ * @param {string} text The text to be cleaned
+ * @returns {Promise<string>}
+ * @description Cleans the text of any sensitive information
+ * @example const cleanText = client.clean(client, text);
+**/
+async function clean(client, text){
+	if(text && text.constructor.name == "Promise") text = await text;
+	if(typeof evaled !== "string") text = require("util").inspect(text, { depth: 0 });
 
-		const emojiSplit = emojiID.split(":");
-		emojiID = emojiSplit[emojiSplit.length - 1];
+	text = text
+		.replace(/`/g, "`" + String.fromCharCode(8203))
+		.replace(/@/g, "@" + String.fromCharCode(8203))
+		.replace(client.token, "https://i.imgur.com/cGIay9e.png");
 
-		return (client.emojis.cache.get(emojiID)) ? client.emojis.cache.get(emojiID) : undefined;
-	};
+	return text;
+}
 
 
-	// Handle the unhandled things
-	process.on("uncaughtException", (err) => {
-		const errorMsg = err?.stack?.replace(new RegExp(`${__dirname}/`, "g"), "./");
-		client.output("error", `Uncaught Exception: ${errorMsg}`);
-	});
+/**
+ * @name grabChannel
+ * @param {string} channelID The ID of the channel to be grabbed
+ * @returns {Promise<Channel | undefined>} The channel object or undefined if the channel is not found
+ * @description Grabs a channel object from the cache or fetches it from the API
+ * @example const channel = grabChannel("1234567890");
+**/
+async function grabChannel(client, channelID){
+	if(!channelID) return undefined;
+	if(channelID.startsWith("<#") && channelID.endsWith(">")) channelID = channelID.slice(2, -1);
+	if(client.channels.cache.get(channelID)) return client.channels.cache.get(channelID);
 
-	process.on("unhandledRejection", (err) => {
-		client.output("error", `Unhandled rejection: ${err}`);
-	});
+	await client.channels.fetch(channelID).catch(e => { return undefined; });
+
+	return (client.channels.cache.get(channelID)) ? client.channels.cache.get(channelID) : undefined;
+}
+
+
+/**
+ * @name grabUser
+ * @param {Client} client The client object
+ * @param {string} userID The ID of the user to be grabbed
+ * @returns {Promise<User | undefined>} The user object or undefined if the user is not found
+ * @description Grabs a user object from the cache or fetches it from the API
+ * @example const user = grabUser("1234567890");
+**/
+async function grabUser(client, userID){
+	if(!userID) return undefined;
+	if(userID.startsWith("<@") && userID.endsWith(">")) userID = userID.slice(2, -1);
+	if(userID.startsWith("!")) userID = userID.slice(1);
+
+	if(client.users.cache.get(userID)) return client.users.cache.get(userID);
+
+	await client.users.fetch(userID).catch(e => { return undefined; });
+
+	return (client.users.cache.get(userID)) ? client.users.cache.get(userID) : undefined;
+}
+
+
+/**
+ * @name grabEmoji
+ * @param {Client} client The client object
+ * @param {string} emojiID The ID of the emoji to be grabbed
+ * @returns {Promise<Emoji | undefined>} The emoji object or undefined if the emoji is not found
+ * @description Grabs a emoji object from the cache via it's ID
+ * @example const emoji = grabEmoji("1234567890");
+**/
+async function grabEmoji(client, emojiID){
+	if(!emojiID) return undefined;
+	if(emojiID.startsWith("<a:") && emojiID.endsWith(">")) emojiID = emojiID.slice(3, -1);	// Animated
+	if(emojiID.startsWith("<:") && emojiID.endsWith(">")) emojiID = emojiID.slice(2, -1);	// Static
+
+	const emojiSplit = emojiID.split(":");
+	emojiID = emojiSplit[emojiSplit.length - 1];
+
+	return (client.emojis.cache.get(emojiID)) ? client.emojis.cache.get(emojiID) : undefined;
+}
+
+
+/**
+ * @typedef {object} messageData
+ * @property {string} content A string of the message
+ * @property {Embed[]} embeds
+ * @property {array} components
+ **/
+
+
+/**
+ * @name handleElement
+ * @param {Message|ChatInputCommandInteraction} element The message or interaction that was created
+ * @param {bool} slashCommand Whether or not the command is a slash command
+ * @param {messageData} messsgeData The data to send
+ * @returns {Message|InteractionResponse}
+**/
+async function handleElement(element, slashCommand, messageData, deferred = false){
+
+	if(!messageData?.content && !messageData?.embeds) throw new Error().name("NoContentError").message("No content or embeds were provided");
+
+	const data = {};
+	if(messageData.content)	data.content = messageData.content.toString();
+	if(messageData.embeds) data.embeds = messageData.embeds;
+	if(messageData.components) data.components = messageData.components;
+	if(slashCommand && messageData.ephemeral && !deferred) data.ephemeral = messageData.ephemeral;
+
+	let reply;
+	if(deferred){
+		reply = await element.editReply(data);
+	} else {
+		reply = await element.reply(data);
+	}
+
+	return reply;
+}
+
+
+/**
+ * @name caseFix
+ * @param {String} string The string to be fixed
+ * @returns {string} The string with correct natural capitalization
+ * @description Fixes the capitalization of a string
+ * @example const fixed = caseFix("this is a StrinG. thE capitaLIZaTion iS wrOng.");
+ * console.log(fixed); // This is a string. The capitalization is wrong.
+**/
+function caseFix(string){
+	string = string.toString();
+
+	const stringArray = string.toLowerCase().split(/\s+/g);
+	const newStringArray = [];
+
+	let capNext = true;
+	for(const word of stringArray){
+		let s = word;
+		if(capNext) s = word.charAt(0).toUpperCase() + word.slice(1);
+		newStringArray.push(s);
+
+		capNext = (word.includes(".")) ? true : false;
+	}
+
+	const newString = newStringArray.join(" ");
+	return newString;
+}
+
+
+/**
+ * @name humanTime
+ * @param {Number} ms The number of milliseconds to convert
+ * @param {String} [format] The format to convert the time to, defaults to "\\h hours \\m minutes \\s seconds"
+ * @returns {String} The converted time
+ * @description Convert milliseconds to human readable formats
+ * - `\\S` = Miliseconds
+ * - `\\s` = Seconds
+ * - `\\m` = Minutes
+ * - `\\h` or `\\H` = Hours
+ * - `\\d` or `\\D` = Days
+ * - `\\M` = Months
+ * - `\\y` or `\\Y` = Years
+ * @example const format = humanTime(951000, "\\m minutes \\s seconds");
+ * console.log(format); // 15 minutes 51 seconds
+**/
+function humanTime(ms, format = "\\h hours \\m minutes \\s seconds"){
+
+	if(!format) return `${ms}ms`;
+
+	const baseMilliseconds = ms;
+	const baseSeconds = Math.floor(baseMilliseconds / 1000);
+	const baseMinutes = Math.floor(baseSeconds / 60);
+	const baseHours = Math.floor(baseMinutes / 60);
+	const baseDays = Math.floor(baseHours / 24);
+	const baseMonths = Math.floor(baseDays / 30);
+	const baseYears = Math.floor(baseDays / 365);
+
+	let milliseconds = baseMilliseconds;
+	let seconds = baseSeconds;
+	let minutes = baseMinutes;
+	let hours = baseHours;
+	let days = baseDays;
+	let months = baseMonths;
+	const years = baseYears;
+
+	if(format.includes("\\s")) milliseconds = baseMilliseconds % 1000;					// Seconds
+	if(format.includes("\\m")) seconds = baseSeconds % 60;								// Minutes
+	if(format.includes("\\h") || format.includes("\\H")) minutes = baseMinutes % 60;	// Hours
+	if(format.includes("\\d") || format.includes("\\D")) hours = baseHours % 24;		// Days
+	if(format.includes("\\M")) days = baseDays % 30;									// Months
+	if(format.includes("\\y") || format.includes("\\Y")) months = months % 12;			// Years
+
+	const duration = format.replace(/\\S/, milliseconds)
+		.replace(/\\s/g, seconds)
+		.replace(/\\m/g, minutes)
+		.replace(/\\h/g, hours)
+		.replace(/\\H/g, hours)
+		.replace(/\\d/g, days)
+		.replace(/\\D/g, days)
+		.replace(/\\M/g, months)
+		.replace(/\\y/g, years)
+		.replace(/\\Y/g, years);
+
+	return duration;
+}
+
+
+module.exports = {
+	ordinal,
+	timeFormat,
+	output,
+	permLevel,
+	clean,
+	grabChannel,
+	grabUser,
+	grabEmoji,
+	handleElement,
+	caseFix,
+	humanTime
 };
