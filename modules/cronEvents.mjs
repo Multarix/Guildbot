d7c3365bcee982a5519aed518d4395ba8c88725c
Multarix <darien2105@gmail.com>
2023-05-08 11:57:47 +1000

    Disable book alerts for now

--- modules/cronEvents.mjs
+++ modules/cronEvents.mjs
@@ -1,5 +1,5 @@
 import dailyWeather from "./dailyWeather.mjs";
-import bookAlerts from "./bookAlerts.mjs";
+// import bookAlerts from "./bookAlerts.mjs";
 
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
@@ -8,9 +8,9 @@ const cron = require("node-cron");
 
 async function cronEvents(client){
 	// Book Alerts
-	cron.schedule("0/15 0-1 0-14 * * 2-6", async () => {
-		bookAlerts(client);
-	});
+	// cron.schedule("0/15 0-1 0-14 * * 2-6", async () => {
+	// 	bookAlerts(client);
+	// });
 
 	// Weather Data
 	try {
