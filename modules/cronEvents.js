763cec6ac5e662166756dbe5fbc86342ed26a815
Multarix <darien2105@gmail.com>
2025-05-30 19:04:25 +1000

    Cron events info

--- modules/cronEvents.js
+++ modules/cronEvents.js
@@ -5,8 +5,8 @@ import bookAlerts from "./bookAlerts.js";
 
 
 async function cronEvents(client){
-	if(!process.env.bookUpdateURL || process.env.bookUpdateURL === "jnovel json feed url") return;
-	if(!process.env.bookUpdatesChannel || process.env.bookUpdatesChannel === "channel id") return;
+	if(!process.env.bookUpdateURL || process.env.bookUpdateURL === "jnovel json feed url") return client.log("error", "Book Update URL was not set, skipping cron events.");
+	if(!process.env.bookUpdatesChannel || process.env.bookUpdatesChannel === "channel id") return client.log("error", "Book Update Channel was not set, skipping cron events.");
 	cron.schedule("0/15 0-1 0-14 * * 2-6", bookAlerts.bind(null, client));
 }
 
